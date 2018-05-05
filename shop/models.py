4e7d108f8baab153a2a22dc0bf4b200cbc7f0f2a
Moh'd Salem <m_saady_salem@msn.com>
2018-05-05 17:37:17 +0300

    Add __str__() method to shop app models

--- shop/models.py
+++ shop/models.py
@@ -4,10 +4,20 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
     slug = models.SlugField(unique=True, blank=True)
 
     class Meta:
         ordering = ('name', '-created_at',)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -18,7 +28,17 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     slug = models.SlugField(unique=True, blank=True)
 
-    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('name', 'price', '-created_at')
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+    def __str__(slef):
+        return self.name

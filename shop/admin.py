9c6c48578da36bdb6d69dbe0566f7b7620be0b3f
Moh'd Salem <m_saady_salem@msn.com>
2018-05-06 08:58:00 +0300

    Customize shop models in admin site
    Add discount 20% action
    Customize category model in admin site

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,37 @@ from django.contrib import admin
 
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+# Admin Site Customization
+admin.site.site_header = "Coretabe Online Shop Administration"
+admin.site.site_title = "Coretabs Online Shop Administration"
+admin.site.index_title = ""  # Change "Site Administration" to an empty text.
+
+
+# ModelAdmin Customization
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"  # Add timeline to Products
+    search_fields = ['name']
+    list_display = ('name', 'price', 'stock', 'category',)
+    list_filter = ('created_at', 'category')
+    actions = ['make_price_zero', 'discount_20']
+
+    # Actions
+    def make_price_zero(self, request, queryset):
+        queryset.update(price=0)
+    make_price_zero.short_description = "Make selected products free"
+
+    def discount_20(self, request, queryset):
+        for product in queryset:
+            new_price = product.price * 80 / 100
+            queryset.update(price=new_price)
+
+    discount_20.short_description = "Discount 20%% for selected products"
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    search_fields = ['name']
+    list_display = ('name', 'description')
